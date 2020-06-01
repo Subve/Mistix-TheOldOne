9b0aaba04c3e60dfee33b0744233fde4a193c104
Subve <60174799+Subve@users.noreply.github.com>
2020-06-01 15:31:59 +0200

    Adding the whole project

---
+++ Mistix/Headers.h
@@ -0,0 +1,16 @@
+#pragma once
+#include <SFML/Window.hpp>
+#include <SFML/Graphics.hpp>
+#include<SFML/System.hpp>
+#include <vector>
+#include <unordered_map>
+#include <functional>
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include"EventManager.h"
+#include"Game.h"
+#include"StateManager.h"
+#include"Window.h"
+#include"BaseState.h"
+#include"SharedContext.h"
